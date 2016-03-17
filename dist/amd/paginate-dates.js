bdc22be699ac0c664953b10e3d386dae3173627a
Marshall Thompson <marshall@creativeideal.net>
2016-03-17 09:16:41 -0600

    0.1.0

---
+++ dist/amd/paginate-dates.js
@@ -0,0 +1,97 @@
+/*paginate-dates@0.0.1#paginate-dates*/
+define([
+    'exports',
+    'can/component',
+    'can/map',
+    'can/map/define',
+    'css!./paginate-dates.less.css',
+    'moment'
+], function (exports, _canComponent, _canMap, _canMapDefine, _paginateDatesLess, _moment) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : { 'default': obj };
+    }
+    var _Component = _interopRequireDefault(_canComponent);
+    var _Map = _interopRequireDefault(_canMap);
+    var _moment2 = _interopRequireDefault(_moment);
+    var ViewModel = _Map['default'].extend({
+        define: {
+            targetDate: {
+                Type: _moment2['default'],
+                value: _moment2['default'].now()
+            },
+            startDate: {
+                Type: _moment2['default'],
+                get: function get() {
+                    var interval = this.attr('interval'), multiplier = this.attr('multiplier'), projectionType = this.attr('projectionType');
+                    if (projectionType === 'back') {
+                        return this.attr('targetDate').clone().subtract(multiplier - 1, interval).startOf(interval);
+                    } else {
+                        return this.attr('endDate').clone().subtract(multiplier - 1, interval).startOf(interval);
+                    }
+                }
+            },
+            endDate: {
+                Type: _moment2['default'],
+                get: function get() {
+                    var interval = this.attr('interval'), multiplier = this.attr('multiplier'), projectionType = this.attr('projectionType');
+                    if (projectionType === 'back') {
+                        return this.attr('startDate').clone().add(multiplier - 1, interval).endOf(interval);
+                    } else {
+                        return this.attr('targetDate').clone().add(multiplier - 1, interval).endOf(interval);
+                    }
+                }
+            },
+            numberOfDays: {
+                get: function get() {
+                    var start = this.attr('startOfFirstWeek'), end = this.attr('endOfLastWeek');
+                    return end.clone().diff(start, 'days') + 1;
+                }
+            },
+            projectionType: {
+                value: 'back',
+                set: function set(val) {
+                    if (val !== 'back' && val !== 'forward') {
+                        val = 'back';
+                    }
+                    return val;
+                }
+            },
+            multiplier: { value: 2 },
+            interval: { value: 'week' },
+            intervalTypes: {
+                value: [
+                    'day',
+                    'week',
+                    'month',
+                    'quarter',
+                    'year'
+                ]
+            }
+        },
+        incrementTargetDate: function incrementTargetDate() {
+            var target = this.attr('targetDate');
+            var multiplier = this.attr('multiplier');
+            var interval = this.attr('interval');
+            this.attr('targetDate', target.clone().add(multiplier, interval));
+        },
+        decrementTargetDate: function decrementTargetDate() {
+            var target = this.attr('targetDate');
+            var multiplier = this.attr('multiplier');
+            var interval = this.attr('interval');
+            this.attr('targetDate', target.clone().subtract(multiplier, interval));
+        },
+        incrementMultiplier: function incrementMultiplier() {
+            this.attr('multiplier', this.attr('multiplier') + 1);
+        },
+        decrementMultiplier: function decrementMultiplier() {
+            this.attr('multiplier', this.attr('multiplier') - 1 || 1);
+        }
+    });
+    exports.ViewModel = ViewModel;
+    exports['default'] = _Component['default'].extend({
+        tag: 'paginate-dates',
+        viewModel: ViewModel
+    });
+});
---
+++ dist/cjs/paginate-dates.js
@@ -0,0 +1,93 @@
+/*paginate-dates@0.0.1#paginate-dates*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { 'default': obj };
+}
+var _canComponent = require('can/component/component');
+var _canComponent2 = _interopRequireDefault(_canComponent);
+var _canMap = require('can/map/map');
+var _canMap2 = _interopRequireDefault(_canMap);
+require('can/map/define/define');
+require('./paginate-dates.less.css');
+var _moment = require('moment');
+var _moment2 = _interopRequireDefault(_moment);
+var ViewModel = _canMap2['default'].extend({
+    define: {
+        targetDate: {
+            Type: _moment2['default'],
+            value: _moment2['default'].now()
+        },
+        startDate: {
+            Type: _moment2['default'],
+            get: function get() {
+                var interval = this.attr('interval'), multiplier = this.attr('multiplier'), projectionType = this.attr('projectionType');
+                if (projectionType === 'back') {
+                    return this.attr('targetDate').clone().subtract(multiplier - 1, interval).startOf(interval);
+                } else {
+                    return this.attr('endDate').clone().subtract(multiplier - 1, interval).startOf(interval);
+                }
+            }
+        },
+        endDate: {
+            Type: _moment2['default'],
+            get: function get() {
+                var interval = this.attr('interval'), multiplier = this.attr('multiplier'), projectionType = this.attr('projectionType');
+                if (projectionType === 'back') {
+                    return this.attr('startDate').clone().add(multiplier - 1, interval).endOf(interval);
+                } else {
+                    return this.attr('targetDate').clone().add(multiplier - 1, interval).endOf(interval);
+                }
+            }
+        },
+        numberOfDays: {
+            get: function get() {
+                var start = this.attr('startOfFirstWeek'), end = this.attr('endOfLastWeek');
+                return end.clone().diff(start, 'days') + 1;
+            }
+        },
+        projectionType: {
+            value: 'back',
+            set: function set(val) {
+                if (val !== 'back' && val !== 'forward') {
+                    val = 'back';
+                }
+                return val;
+            }
+        },
+        multiplier: { value: 2 },
+        interval: { value: 'week' },
+        intervalTypes: {
+            value: [
+                'day',
+                'week',
+                'month',
+                'quarter',
+                'year'
+            ]
+        }
+    },
+    incrementTargetDate: function incrementTargetDate() {
+        var target = this.attr('targetDate');
+        var multiplier = this.attr('multiplier');
+        var interval = this.attr('interval');
+        this.attr('targetDate', target.clone().add(multiplier, interval));
+    },
+    decrementTargetDate: function decrementTargetDate() {
+        var target = this.attr('targetDate');
+        var multiplier = this.attr('multiplier');
+        var interval = this.attr('interval');
+        this.attr('targetDate', target.clone().subtract(multiplier, interval));
+    },
+    incrementMultiplier: function incrementMultiplier() {
+        this.attr('multiplier', this.attr('multiplier') + 1);
+    },
+    decrementMultiplier: function decrementMultiplier() {
+        this.attr('multiplier', this.attr('multiplier') - 1 || 1);
+    }
+});
+exports.ViewModel = ViewModel;
+exports['default'] = _canComponent2['default'].extend({
+    tag: 'paginate-dates',
+    viewModel: ViewModel
+});
