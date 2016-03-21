d5037224d758ae68841ef138a533985ac8a09302
Marshall Thompson <marshall@creativeideal.net>
2016-03-20 23:19:18 -0600

    Added a function to set the target date to a specific date.
    Also added some documentation.

--- src/paginate-dates.js
+++ src/paginate-dates.js
@@ -6,6 +6,10 @@ import moment from 'moment';
 
 export const ViewModel = Map.extend({
   define: {
+    /**
+     * The `targetDate` is currently the master date.  The `startDate` and `endDate`
+     * both use the `targetDate` for their calculations.
+     */
     targetDate: {
       Type: moment,
       value: moment.now()
@@ -38,6 +42,10 @@ export const ViewModel = Map.extend({
         }
       }
     },
+    /**
+     * `numberOfDays` is simply the integer count of the number of days in the
+     * selected period.
+     */
     numberOfDays: {
       get(){
         let start = this.attr('startDate'),
@@ -45,6 +53,13 @@ export const ViewModel = Map.extend({
         return end.clone().diff(start, 'days') + 1;
       }
     },
+
+    /**
+     * The `projectionType` determines which direction the module displays additional
+     * data above 1 day.  It can be either 'back' or 'forward'.  If it's 'back', it
+     * will calculate back in time.  If it's set to 'forward', it will calculate
+     * forward in time.
+     */
     projectionType: {
       value: 'back',
       set(val){
@@ -64,23 +79,47 @@ export const ViewModel = Map.extend({
       value: ['day', 'week', 'month', 'quarter', 'year']
     }
   },
+  /**
+   * `incrementTargetDate` adds time to the `targetDate` according to the current
+   * multiplier and interval.
+   */
   incrementTargetDate(){
     let target = this.attr('targetDate');
     let multiplier = this.attr('multiplier');
     let interval = this.attr('interval');
     this.attr('targetDate', target.clone().add(multiplier, interval));
   },
+  /**
+   * `decrementTargetDate` removes time from the `targetDate` according to the current
+   * multiplier and interval.
+   */
   decrementTargetDate(){
     let target = this.attr('targetDate');
     let multiplier = this.attr('multiplier');
     let interval = this.attr('interval');
     this.attr('targetDate', target.clone().subtract(multiplier, interval));
   },
+
+  /**
+   * `incrementMultiplier` increases the multiplier by 1.  For example 1 week
+   * would become 2 weeks. It will always be at least 1.
+   */
   incrementMultiplier(){
     this.attr('multiplier', this.attr('multiplier') + 1);
   },
+
+  /**
+   * `decrementMultiplier` decreases the multiplier by 1.  For example 2 weeks
+   * would become 1 weeks. It cannot be dropped below 1.
+   */
   decrementMultiplier(){
     this.attr('multiplier', this.attr('multiplier') - 1 || 1);
+  },
+  /**
+   * `setTargetDate` allows you to directly set the `targetDate` to a specific date.
+   */
+  setTargetDate(date){
+    this.attr('targetDate', date);
   }
 });
 
